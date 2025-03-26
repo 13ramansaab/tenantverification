096b4f9fdce561ae7856e375267300c296100459
Raman <ramanmunday13@gmail.com>
2025-03-26 13:22:57 -0400

    yes

--- functions/verify-payment.js
+++
@@ -1,9 +0,0 @@
-// netlify/functions/verify-payments.js
-exports.handler = async (event) => {
-  console.log('Function invoked:', event);
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ message: 'Hello from verify-payments', orderId: event.queryStringParameters?.orderId }),
-  };
-};
