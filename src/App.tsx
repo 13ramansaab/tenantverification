7190b180955b707f4f0d6243909f4f6dd89bc398
Raman <ramanmunday13@gmail.com>
2025-03-26 09:11:19 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import ContactUs from './components/ContactUs';
+import TermsAndConditions from './components/TermsAndConditions';
 import PaymentSuccess from './components/PaymentSuccess';
+import PaymentCancel from './components/PaymentCancel';
+import SuccessPage from './components/SuccessPage';
+
 
 interface RegistrationFormProps {
   onPaymentComplete: () => Promise<void>;
